54633692182710fc363cd4bc19cfc9dd67eeda2b
yanis lamliji <yanis.lamliji@ynov.com>
2025-06-02 12:59:47 +0200

    Beta Code

--- Direction.py
+++ Direction.py
@@ -1,15 +1,19 @@
 from machine import Pin
 
 class Component():
-    def __init__(self, wheel_Left, wheel_Right):
-        self.wheel_Left = wheel_Left
-        self.wheel_Right = wheel_Right
+    def __init__(self, wheel_Left_Back, wheel_Right_Back, wheel_Left_Prior, wheel_Right_Prior):
+        self.wheel_Left_back = wheel_Left_Back
+        self.wheel_Right_back = wheel_Right_Back
+        self.wheel_Left_Prior = wheel_Left_Prior
+        self.wheel_Right_Prior = wheel_Right_Prior
 
 class Robot(Component):
     def __init__(self,pin):
         super.__init__(pin)
 
     
-    def move(self, value):
-        self.wheel_Left.value(value)
-        self.wheel_Right.value(value)
+    def move(self, Value_Left, Value_Right):
+        self.wheel_Left.value(Value_Left)
+        self.wheel_Right.value(Value_Right)
+        self.wheel_Left.value(Value_Left)
+        self.wheel_Right.value(Value_Right)
